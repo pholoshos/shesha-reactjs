a83784debf9355aaf6b6c573896f251cb5f07188
Luke <Lukeybooi@gmail.com>
2022-03-28 17:18:53 +0200

    Added checkBoxGroup utils

---
+++ src/components/formDesigner/components/checkboxGroup/utils.ts
@@ -0,0 +1,20 @@
+import { SpaceProps } from 'antd';
+import { IConfigurableFormComponent } from '../../../../providers/form/models';
+
+export interface ICheckItem {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export type DataSourceType = 'values' | 'referenceList';
+
+export interface ICheckboxGoupProps extends IConfigurableFormComponent {
+  items?: ICheckItem[];
+  mode?: 'single' | 'multiple';
+  referenceListNamespace?: string;
+  referenceListName?: string;
+  dataSourceType: DataSourceType;
+  orientation?: SpaceProps['direction'];
+  values?: ICheckItem[];
+}

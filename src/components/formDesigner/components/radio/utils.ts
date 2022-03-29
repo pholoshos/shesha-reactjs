5737522c49e2a26e055c75e94360941cd85190ab
Luke <Lukeybooi@gmail.com>
2022-03-29 17:27:15 +0200

    Added getDataSourceList function and clean-up on FormDesigner Radio utils

--- src/components/formDesigner/components/radio/utils.ts
+++ src/components/formDesigner/components/radio/utils.ts
@@ -1,16 +1,33 @@
-import { SpaceProps } from "antd";
-import { IConfigurableFormComponent } from "../../../../interfaces";
-
-export interface IRadioItems {
-  id: string;
-  name: string;
-  value: string;
-}
-
-export type DataSourceType = 'values' | 'reflist';
+import { SpaceProps } from 'antd';
+import { ReferenceListItemDto } from '../../../../apis/referenceList';
+import { IConfigurableFormComponent } from '../../../../interfaces';
+import { DataSourceType, ILabelValue } from '../dropdown/models';
 
 export interface IRadioProps extends IConfigurableFormComponent {
-  items?: IRadioItems[];
+  items?: ILabelValue[];
+  referenceListNamespace?: string;
+  referenceListName?: string;
   dataSourceType: DataSourceType;
   direction?: SpaceProps['direction'];
 }
+
+export const getDataSourceList = (
+  dataSource: DataSourceType,
+  values: ILabelValue[],
+  refList: ReferenceListItemDto[],
+  urlList: any[] = []
+): ILabelValue[] => {
+  switch (dataSource) {
+    case 'values':
+      return values;
+
+    case 'referenceList':
+      return (refList || []).map(({ id, item, itemValue }) => ({ id, value: itemValue, label: item }));
+    case 'url':
+      return (urlList || []).map(i => ({
+        id: i?.id,
+        value: i?.id || i?.itemValue,
+        label: i?.displayName || i?.name || i?.item,
+      }));
+  }
+};

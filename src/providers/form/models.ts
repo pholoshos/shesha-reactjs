6f51c58e7d6e4450b01010211074b305cdd5fd6c
Luke <Lukeybooi@gmail.com>
2022-04-27 18:37:11 +0200

    feat: added IFormValidationRulesOptions interface

--- src/providers/form/models.ts
+++ src/providers/form/models.ts
@@ -217,3 +217,7 @@ export interface IFormSection {
 export interface IFormDto extends Omit<FormDto, 'markup'> {
   markup: FormMarkupWithSettings;
 }
+
+export interface IFormValidationRulesOptions {
+  formData?: any;
+}

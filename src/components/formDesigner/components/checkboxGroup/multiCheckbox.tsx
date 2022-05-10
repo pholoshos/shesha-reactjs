48c0cd1f2086e843ba037adebf7cf5053d56f71b
Luke <Lukeybooi@gmail.com>
2022-05-10 22:23:30 +0200

    fix: multicheck disable fix

--- src/components/formDesigner/components/checkboxGroup/multiCheckbox.tsx
+++ src/components/formDesigner/components/checkboxGroup/multiCheckbox.tsx
@@ -1,12 +1,15 @@
 import { Checkbox, Col, Row } from 'antd';
 import React, { FC, useEffect, useState } from 'react';
 import { ReferenceListItemDto, useReferenceListGetItems } from '../../../../apis/referenceList';
+import { useForm } from '../../../../providers';
 import { getCachedItems, saveListItems } from '../../../refListDropDown/utils';
 import { getDataSourceList } from '../radio/utils';
 import { getSpan, ICheckboxGroupProps } from './utils';
 
 const MultiCheckbox: FC<ICheckboxGroupProps> = model => {
   const { items, referenceListName, referenceListNamespace, direction, value, onChange } = model;
+
+  const { formMode } = useForm();
 
   const { refetch: fetchItems, data: listItemsResult } = useReferenceListGetItems({
     lazy: true,
@@ -36,12 +39,14 @@ const MultiCheckbox: FC<ICheckboxGroupProps> = model => {
 
   const options = getDataSourceList(model?.dataSourceType, items, listItems) || [];
 
+  const isReadOnly = model?.readOnly || formMode === 'readonly';
+
   return (
     <Checkbox.Group value={value} onChange={onChange} style={model?.style}>
       <Row>
         {options.map(({ id, label, value: v }) => (
           <Col id={id} span={getSpan(direction, options.length)}>
-            <Checkbox id={id} value={v}>
+            <Checkbox id={id} value={v} disabled={isReadOnly}>
               {label}
             </Checkbox>
           </Col>

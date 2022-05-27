22a55bb0f0e02721be23db933aa8fc83ec6c63a6
Phil <84430458+PhilMashiane@users.noreply.github.com>
2022-05-27 11:06:50 +0200

    fix: child table issues that causes panel to collapse on keyPress

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -29,6 +29,9 @@ export const CollapsiblePanel: FC<ICollapsiblePanelProps> = ({
   className,
   style,
 }) => {
+  // Prevent the CollapsiblePanel from collapsing every time you click anywhere on the extra and header
+  const onContainerClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => event?.stopPropagation();
+
   return (
     <Collapse
       defaultActiveKey={['1']}
@@ -37,7 +40,15 @@ export const CollapsiblePanel: FC<ICollapsiblePanelProps> = ({
       className={classNames('sha-collapsible-panel', className, { 'no-content-padding': noContentPadding })}
       style={style}
     >
-      <Panel header={<span className={`ant-collapse-header-text`}>{header || ' '}</span>} key="1" extra={extra}>
+      <Panel
+        header={
+          <span className={`ant-collapse-header-text`} onClick={onContainerClick}>
+            {header || ' '}
+          </span>
+        }
+        key="1"
+        extra={<span onClick={onContainerClick}>{extra}</span>}
+      >
         <Skeleton loading={loading}>{children}</Skeleton>
       </Panel>
     </Collapse>

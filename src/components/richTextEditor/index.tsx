e1279b12b00ded33dd55ec29b91a7a90e5add8b0
Phil <84430458+PhilMashiane@users.noreply.github.com>
2022-06-02 16:20:54 +0200

    fix(JoditReact): issue with undefined window at build time

--- src/components/richTextEditor/index.tsx
+++ src/components/richTextEditor/index.tsx
@@ -1,25 +1,30 @@
 import React, { CSSProperties, FC, useEffect, useRef, useState } from 'react';
-import JoditEditor, { JoditProps } from 'jodit-react';
 import classNames from 'classnames';
-import Show from '../show';
+import { Skeleton } from 'antd';
 
-export interface IRichTextEditorProps extends Omit<JoditProps, 'value'> {
+const JoditEditor = React.lazy(() => {
+  return import('jodit-react');
+});
+
+export interface IRichTextEditorProps {
   value?: string;
   onChange?: (value: string) => void;
+  onBlur?: (value: string) => void;
+  config?: any;
   className?: string;
   style?: CSSProperties;
 }
 
 interface IRichTextEditorState {
-  content?: any;
-  hasWindow?: boolean;
+  content?: string;
 }
 
 export const RichTextEditor: FC<IRichTextEditorProps> = ({ value, onChange, config, style, className }) => {
+  const isSSR = typeof window === 'undefined';
   const editor = useRef(null);
-  const [state, setState] = useState<IRichTextEditorState>({ content: value, hasWindow: false });
+  const [state, setState] = useState<IRichTextEditorState>({ content: value });
 
-  const { hasWindow, content } = state;
+  const { content } = state;
 
   useEffect(() => {
     setState(prev => ({ ...prev, hasWindow: true }));
@@ -33,17 +38,19 @@ export const RichTextEditor: FC<IRichTextEditorProps> = ({ value, onChange, conf
     }
   };
 
-  return (
-    <div style={style} className={classNames('sha-rich-text-editor', className)}>
-      <Show when={hasWindow}>
+  return isSSR ? (
+    <Skeleton loading={true} />
+  ) : (
+    <React.Suspense fallback={<div>Loading editor...</div>}>
+      <div style={style} className={classNames('sha-rich-text-editor', className)}>
         <JoditEditor
           ref={editor}
           value={content}
           config={config}
           onBlur={handleChange} // preferred to use only this option to update the content for performance reasons
         />
-      </Show>
-    </div>
+      </div>
+    </React.Suspense>
   );
 };
 

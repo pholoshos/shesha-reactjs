2b440b40cc764b58de705c2c9bce489adee546e0
Phil <84430458+PhilMashiane@users.noreply.github.com>
2022-08-18 14:24:24 +0200

    fix(lint): Build error 'semantic error TS2698: Spread types may only be created from object types.'

--- src/components/configurableForm/configurableFormRenderer.tsx
+++ src/components/configurableForm/configurableFormRenderer.tsx
@@ -146,7 +146,7 @@ export const ConfigurableFormRenderer: FC<IConfigurableFormRendererProps> = ({
     }
   }, [getUrl, formData, globalState, parentFormValues, skipFetchData]);
 
-  const fetchedFormEntity = fetchedEntity?.result;
+  const fetchedFormEntity = typeof fetchedEntity?.result === 'object' ? fetchedEntity?.result : {};
 
   useEffect(() => {
     if (fetchedFormEntity && onDataLoaded) {
@@ -176,7 +176,7 @@ export const ConfigurableFormRenderer: FC<IConfigurableFormRendererProps> = ({
     // If you want only `fetchedFormEntity`, don't pass `initialValuesFromSettings`
     if (!_.isEmpty(initialValuesFromSettings)) {
       incomingInitialValues = fetchedFormEntity
-        ? { ...fetchedFormEntity, ...initialValuesFromSettings }
+        ? { ...(typeof fetchedFormEntity === 'object' ? fetchedFormEntity : {}), ...initialValuesFromSettings }
         : initialValuesFromSettings;
     } else if (!_.isEmpty(fetchedFormEntity) || !_.isEmpty(lastTruthyPersistedValue)) {
       // `fetchedFormEntity` will always be merged with persisted values from local storage

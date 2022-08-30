900603fc495c0b4fe3f42dc3d20e27f504733a36
Luke <Lukeybooi@gmail.com>
2022-08-31 00:44:29 +0200

    feat: added setMultiSelectedRow to dataTableSelection contexts

--- src/providers/dataTableSelection/contexts.ts
+++ src/providers/dataTableSelection/contexts.ts
@@ -1,17 +1,22 @@
 import { createContext } from 'react';
+import { Row } from 'react-table';
 import { ISelectionProps } from './models';
 
 export interface IDataTableSelectionStateContext {
   selectedRow?: ISelectionProps;
+  selectedRows?: { [key in string]: string }[];
 }
 
 export interface IDataTableSelectionActionsContext {
   setSelectedRow: (index: number, row: any) => void;
+  setMultiSelectedRow: (rows: Row[] | Row) => void;
+
   /* NEW_ACTION_ACTION_DECLARATION_GOES_HERE */
 }
 
 export const DATATABLE_SELECTION_CONTEXT_INITIAL_STATE: IDataTableSelectionStateContext = {
   selectedRow: null,
+  selectedRows: [],
 };
 
 export const DataTableSelectionStateContext = createContext<IDataTableSelectionStateContext>(

pholoshos/shesha-reactjs

commit eefe90da6f3e85ee94a0c2ca10c40f15e4dc6845

ci(FormDesigner): Dispacch even on button click Fix Submit list items Fix inline list item adding Passed labelCol and wrapperCol to List and SubForm Providers Moved SubFormProvider to providers

src/components/formDesigner/components/button/configurableButton/index.tsx
@@ -31,21 +31,6 @@ export const ConfigurableButton: FC<IConfigurableButtonProps> = props => {
   const { globalState } = useGlobalState();
   const { publish } = usePubSub();
 
-  const setData = (data: any) => {
-    setFormData({
-      values: data,
-      mergeValues: true,
-    });
-
-    console.log('LOGS:: ConfigurableButton data', data, form, form?.getFieldsValue());
-
-    form?.setFieldsValue(data);
-
-    setTimeout(() => {
-      console.log('LOGS:: ConfigurableButton data after 3 secs', form?.getFieldsValue());
-    }, 3000);
-  };
-
   const executeExpression = (expression: string, result?: any) => {
     if (!expression) {
       console.error('Expected expression to be defined but it was found to be empty.');
@@ -54,11 +39,10 @@ export const ConfigurableButton: FC<IConfigurableButtonProps> = props => {
     }
 
     // tslint:disable-next-line:function-constructor
-    return new Function('data, moment, form, setFormData, formMode, http, result, message, globalState', expression)(
+    return new Function('data, moment, form, formMode, http, result, message, globalState', expression)(
       formData,
       moment,
       form,
-      setData,
       formMode,
       axiosHttp(backendUrl),
       result,
@@ -95,15 +79,15 @@ export const ConfigurableButton: FC<IConfigurableButtonProps> = props => {
       parentFormValues: formData,
       modalConfirmDialogMessage: convertedProps?.modalConfirmDialogMessage,
       onSubmitted: values => {
-        onSuccessScriptExecutor(values);
+        if (props?.onSubmitEvent) {
+          publish(props?.onSubmitEvent, { stateId: props?.uniqueStateId, state: values });
+        }
 
         if (props?.refreshTableOnSuccess) {
           publish(DataTablePubsubConstants.refreshTable, { stateId: props?.uniqueStateId });
         }
 
-        if (props?.refreshListOnSuccess) {
-          publish(ListControlEvents.refreshListItems, { stateId: props?.uniqueStateId });
-        }
+        onSuccessScriptExecutor(values);
       },
       onFailed: onErrorScriptExecutor,
     };
@@ -159,11 +143,22 @@ export const ConfigurableButton: FC<IConfigurableButtonProps> = props => {
       case 'reset':
         form?.resetFields();
         break;
+      case 'dispatchAnEvent': {
+        const eventName =
+          props?.eventName === 'CUSTOM_EVENT' && props?.customEventNameToDispatch
+            ? props?.customEventNameToDispatch
+            : props?.eventName;
+
+        publish(eventName, { stateId: props?.uniqueStateId || 'NO_PROVIDED' });
+        break;
+      }
       case 'executeFormAction':
       case 'customAction':
         if (props?.formAction) {
-          if (props?.formAction !== 'CUSTOM_ACTION') {
-            publish(props?.formAction, { stateId: props?.uniqueStateId || 'NO_PROVIDED' });
+          if (props?.formAction === 'CUSTOM_ACTION') {
+            console.log('props?.formAction: ', props);
+
+            publish(props?.customFormAction, { stateId: props?.uniqueStateId || 'NO_PROVIDED' });
           } else {
             if (props.customFormAction) {
               const actionBody = getAction(props.formComponentId, props.customFormAction);

src/components/formDesigner/components/listControl/index.tsx
@@ -82,6 +82,8 @@ const ListComponent: IToolboxComponent<IListComponentProps> = {
       showPagination: true,
       hideLabel: true,
       uniqueStateId,
+      labelCol: 5,
+      wrapperCol: 5,
       buttons: [
         {
           id: 'PWrW0k2WXPweNfHnZgbsj',
@@ -132,8 +134,7 @@ const ListComponentRender: FC<IListComponentRenderProps> = ({
   properties,
   renderStrategy,
   uniqueStateId,
-  allowSubmit,
-  submitHttpVerb,
+  submitHttpVerb = 'POST',
   onSubmit,
   submitUrl,
   entityType,
@@ -280,22 +281,11 @@ const ListComponentRender: FC<IListComponentRenderProps> = ({
     }
   }, [value, isInDesignerMode]);
 
-  // useEffect(() => {
-  //   if (formData && name) {
-  //     if (formData[name] && !isEqual(value, formData[name])) {
-  //       onChange(formData[name]);
-  //     }
-  //   }
-  // }, [formData]);
-
-  console.log(
-    'LOGS:: name, value, formData, form, form?.getFieldsValue()',
-    name,
-    value,
-    formData,
-    form,
-    form?.getFieldsValue()
-  );
+  useEffect(() => {
+    if (!value) {
+      onChange([]); // Make sure the form is not undefined
+    }
+  }, [value]);
 
   useSubscribe(ListControlEvents.refreshListItems, ({ stateId }) => {
     if (stateId === uniqueStateId) {
@@ -306,6 +296,16 @@ const ListComponentRender: FC<IListComponentRenderProps> = ({
   useSubscribe(ListControlEvents.saveListItems, ({ stateId }) => {
     if (stateId === uniqueStateId) {
       submitListItems();
+    }
+  });
+
+  const debouncedAddItems = useDebouncedCallback(data => {
+    onChange(Array.isArray(value) ? [...value, data] : [data]);
+  }, 300);
+
+  useSubscribe(ListControlEvents.addListItems, ({ stateId, state }) => {
+    if (stateId === uniqueStateId) {
+      debouncedAddItems(state);
     }
   });
 
@@ -334,7 +334,12 @@ const ListComponentRender: FC<IListComponentRenderProps> = ({
   const renderSubForm = (name?: string) => {
     // Note we do not pass the name. The name will be provided by the List component
     return (
-      <SubFormProvider name={name} markup={markup} properties={[]}>
+      <SubFormProvider
+        name={name}
+        markup={markup}
+        properties={[]}
+        {...{ wrapperCol: { span: 16 }, labelCol: { span: 8 } }}
+      >
         <SubForm />
       </SubFormProvider>
     );
@@ -342,11 +347,13 @@ const ListComponentRender: FC<IListComponentRenderProps> = ({
 
   const deleteItem = useCallback(
     (index: number) => {
-      const item = value[index];
+      if (deleteUrl) {
+        const item = value[index];
 
-      deleteHttp('', { queryParams: { id: item?.id || item.Id } }).then(() => {
-        debouncedRefresh();
-      });
+        deleteHttp('', { queryParams: { id: item?.id || item.Id } }).then(() => {
+          debouncedRefresh();
+        });
+      }
     },
     [value]
   );
@@ -355,7 +362,7 @@ const ListComponentRender: FC<IListComponentRenderProps> = ({
     setState(prev => ({ ...prev, quickSearch: text }));
   }, 200);
 
-  const submitListItems = () => {
+  const submitListItems = useDebouncedCallback(() => {
     if (onSubmit) {
       const getOnSubmitPayload = () => {
         // tslint:disable-next-line:function-constructor
@@ -373,7 +380,7 @@ const ListComponentRender: FC<IListComponentRenderProps> = ({
         message.success('Data saved successfully!');
       });
     }
-  };
+  }, 350);
 
   const isSpinning = submitting || fetchingData || isDeleting;
 
@@ -396,7 +403,9 @@ const ListComponentRender: FC<IListComponentRenderProps> = ({
       }
     >
       <Show when={isInDesignerMode && renderStrategy === 'dragAndDrop'}>
-        <ComponentsContainer containerId={containerId} />
+        <SubFormProvider properties={[]} {...{ wrapperCol: { span: 5 }, labelCol: { span: 24 } }}>
+          <ComponentsContainer containerId={containerId} />
+        </SubFormProvider>
       </Show>
       <Show when={isInDesignerMode && renderStrategy === 'externalForm' && Boolean(formPath?.id)}>
         {renderSubForm('__IGNORE__')}

src/providers/subForm/index.tsx
@@ -240,7 +240,10 @@ const SubFormProvider: FC<SubFormProviderProps> = ({
           putData: isUpdating,
         },
         components: state?.components,
-        layout: { labelCol: { span: labelCol }, wrapperCol: { span: labelCol } },
+        layout: {
+          labelCol: markup?.formSettings?.labelCol || labelCol,
+          wrapperCol: markup?.formSettings?.wrapperCol || wrapperCol,
+        },
       }}
     >
       <SubFormActionsContext.Provider

src/components/formDesigner/components/formItem.tsx
@@ -76,8 +76,7 @@ const ConfigurableFormItem: FC<IConfigurableFormItemProps> = ({
       initialValue={initialValue}
       tooltip={model.description}
       rules={isHidden ? [] : getValidationRules(model, { formData })}
-      labelCol={labelCol}
-      wrapperCol={wrapperCol}
+      {...getLayout()}
       style={style}
     >
       {children}

src/components/formDesigner/components/listControl/constants.ts
@@ -1,4 +1,5 @@
 export const ListControlEvents = {
   refreshListItems: 'refreshListItems',
-  saveListItems: 'refreshListItems',
+  saveListItems: 'saveListItems',
+  addListItems: 'addListItems',
 };

src/components/formDesigner/components/listControl/settingsv2.tsx
@@ -79,6 +79,7 @@ export const ListControlSettings: FC<IListControlSettingsProps> = ({ onSave, mod
       <Show when={state?.allowRemoveItems}>
         <FormItem
           name="deleteUrl"
+          label="Delete URL"
           tooltip="The API url that will be used delete the list item. Write the code that returns the string"
         >
           <CodeEditor
@@ -123,6 +124,7 @@ export const ListControlSettings: FC<IListControlSettingsProps> = ({ onSave, mod
           tooltip="Write a code that return tha payload to be sent to the server when submitting this items"
         >
           <CodeEditor
+            label="On Submit"
             mode="dialog"
             setOptions={{ minLines: 20, maxLines: 500, fixedWidthGutter: true }}
             name="onSubmit"
@@ -136,6 +138,43 @@ export const ListControlSettings: FC<IListControlSettingsProps> = ({ onSave, mod
                 description: 'List of items',
                 type: 'array',
               },
+              {
+                id: '788673a5-5eb9-4a9a-a34b-d8cea9cacb3c',
+                name: 'data',
+                description: 'Form data',
+                type: 'object',
+              },
+              {
+                id: '65b71112-d412-401f-af15-1d3080f85319',
+                name: 'globalState',
+                description: 'The global state',
+                type: 'object',
+              },
+              {
+                id: '3633b881-43f4-4779-9f8c-da3de9ecf9b8',
+                name: 'queryParams',
+                description: 'Query parameters',
+                type: 'object',
+              },
+            ]}
+          />
+        </FormItem>
+
+        <FormItem
+          label="Submit URL"
+          name="submitUrl"
+          tooltip="The URL to submit the list items to. This is required"
+          rules={[{ required: true }]}
+        >
+          <CodeEditor
+            mode="dialog"
+            label="Submit URL"
+            setOptions={{ minLines: 20, maxLines: 500, fixedWidthGutter: true }}
+            name="submitUrl"
+            type={''}
+            id={''}
+            description="The URL to submit the list items to"
+            exposedVariables={[
               {
                 id: '788673a5-5eb9-4a9a-a34b-d8cea9cacb3c',
                 name: 'data',
@@ -274,7 +313,7 @@ export const ListControlSettings: FC<IListControlSettingsProps> = ({ onSave, mod
 
       <SectionSeparator sectionName="Layout" />
 
-      <FormItem name="labelCol" label="">
+      <FormItem name="labelCol" label="Label Col">
         <InputNumber min={1} max={24} defaultValue={5} />
       </FormItem>
 

src/providers/buttonGroupConfigurator/models.ts
@@ -19,7 +19,8 @@ export type ButtonActionType =
   | 'submit'
   | 'reset'
   | 'startFormEdit'
-  | 'cancelFormEdit';
+  | 'cancelFormEdit'
+  | 'dispatchAnEvent';
 
 export interface IButtonGroupItemBase {
   id: string;
@@ -46,7 +47,6 @@ export interface IButtonGroupButton extends IButtonGroupItemBase {
   itemSubType: ToolbarItemSubType;
   buttonAction?: ButtonActionType;
   refreshTableOnSuccess?: boolean; // TODO: Remove this and make this logic more generic
-  refreshListOnSuccess?: boolean; // TODO: Remove this and make this logic more generic
   targetUrl?: string;
 
   /**
@@ -68,6 +68,18 @@ export interface IButtonGroupButton extends IButtonGroupItemBase {
   modalFormMode?: FormMode;
   skipFetchData?: boolean;
   submitLocally?: boolean;
+
+  // This is the event that will be triggered once the form has been submitted. The event will be passed this data
+  onSubmitEvent?: string;
+
+  /** An event name to dispatch on the click of a button */
+  eventName?: string;
+
+  /** The string representing a custom event name to dispatch when the button has been dispatched
+   * in case we forgot to include it in the `eventName` dropdown
+   */
+  customEventNameToDispatch?: string;
+
   modalWidth?: number;
   modalActionOnSuccess?: 'keepOpen' | 'navigateToUrl' | 'close' | undefined;
   showConfirmDialogBeforeSubmit?: boolean;

src/providers/subForm/interfaces.ts
@@ -1,3 +1,4 @@
+import { ColProps } from 'antd';
 import { IGuidNullableEntityWithDisplayNameDto } from '../../interfaces/shesha';
 import { IChangeable, IConfigurableFormComponent, IValuable } from '../../interfaces';
 import { IStoredFilter } from '../dataTable/interfaces';
@@ -14,8 +15,8 @@ export interface ISubFormProps extends IValuable, IChangeable {
   submitUrl?: string;
   onSubmit?: string;
   buttons?: any[];
-  labelCol?: number;
-  wrapperCol?: number;
+  labelCol?: ColProps;
+  wrapperCol?: ColProps;
   dataSource?: 'form' | 'api';
   entityType?: string;
   useExpression?: boolean;

commit f4bcc6037ffbee16da4ca8da36670d883c6c0460

fix(code editor): fixed update of code editor after set value to null fix(configurable button) fixed form actions support (broken in the eefe90da6f3e85ee94a0c2ca10c40f15e4dc6845 commit) commented out some logging

src/components/codeEditor/index.tsx
@@ -39,6 +39,7 @@ const AceEditorLazy = React.lazy<typeof ReactAce>(() => new Promise(async resolv
 }));
 
 export const CodeEditor: FC<ICodeEditorProps> = (props) => {
+    console.log('value', props.value)
     const isSSR = typeof window === 'undefined';
 
     const { aceBaseUrl, ...restProps } = props;
@@ -52,6 +53,7 @@ export const CodeEditor: FC<ICodeEditorProps> = (props) => {
                 mode="javascript" 
                 theme="monokai" 
                 {...restProps}
+                value={restProps.value ? restProps.value : ""} // note: have to change null/undefined to empty string to force re-rendering of the editor
             />
         </React.Suspense>
     )

src/components/formDesigner/components/autocomplete/autocomplete.tsx
@@ -120,7 +120,7 @@ const AutocompleteComponent: IToolboxComponent<IAutocompleteProps> = {
       return queryParamObj;
     };
 
-    console.log('LOGS:: filter, evaluatedFilters, getQueryParams(): ', filter, evaluatedFilters, getQueryParams());
+    //console.log('LOGS:: filter, evaluatedFilters, getQueryParams(): ', filter, evaluatedFilters, getQueryParams());
 
     const getFetchedItemData = (
       item: object,

src/components/formDesigner/components/button/configurableButton/index.tsx
@@ -25,7 +25,7 @@ export interface IConfigurableButtonProps extends Omit<IButtonGroupButton, 'styl
 
 export const ConfigurableButton: FC<IConfigurableButtonProps> = props => {
   const { backendUrl } = useSheshaApplication();
-  const { getAction, form, setFormMode, formData, formMode } = useForm();
+  const { /*getAction,*/ form, setFormMode, formData, formMode } = useForm();
   const { router } = useShaRouting();
   const { globalState } = useGlobalState();
   const { publish } = usePubSub();
@@ -159,12 +159,15 @@ export const ConfigurableButton: FC<IConfigurableButtonProps> = props => {
 
             publish(props?.customFormAction, { stateId: props?.uniqueStateId || 'NO_PROVIDED' });
           } else {
+            /*
             if (props.customFormAction) {
               const actionBody = getAction(props.formComponentId, props.customFormAction);
 
               if (actionBody) actionBody();
               else console.warn(`action ${props.customFormAction} not found on the form`);
             } else console.warn('customFormAction is not specified');
+            */
+            publish(props?.formAction, { stateId: props?.uniqueStateId || 'NO_PROVIDED' });
           }
         }
         break;

src/pages/settings/dynamic-theme/index.tsx
@@ -25,7 +25,7 @@ const ConfigurableThemePage: FC<IConfigurableThemePageProps> = () => {
     (colorName: 'primaryColor' | 'errorColor' | 'warningColor' | 'successColor' | 'infoColor') => {
       const initialColor = theme?.application && theme?.application[colorName];
 
-      console.log('LOGS:: renderColor theme: ', initialColor, colorName);
+      //console.log('LOGS:: renderColor theme: ', initialColor, colorName);
 
       return (
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
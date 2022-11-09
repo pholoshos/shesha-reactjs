d1459372aa14897eeba899f9678d9c4f4b41697e
Phil <84430458+PhilMashiane@users.noreply.github.com>
2022-11-09 11:23:49 +0200

    ci(MultiCheckbox): Prevented unnecessary fetch of values

--- src/components/formDesigner/components/checkboxGroup/multiCheckbox.tsx
+++ src/components/formDesigner/components/checkboxGroup/multiCheckbox.tsx
@@ -22,7 +22,7 @@ const MultiCheckbox: FC<ICheckboxGroupProps> = model => {
     })();
   };
 
-  const { refetch, data } = useGet({ path: getEvaluatedUrl(model?.dataSourceUrl) });
+  const { refetch, data } = useGet({ path: getEvaluatedUrl(model?.dataSourceUrl), lazy: true });
 
   useEffect(() => {
     if (model?.dataSourceType === 'url' && model?.dataSourceUrl) {

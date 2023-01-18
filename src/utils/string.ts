b2ca8d3654496824aa6324b4ba7329a9348e4954
Luke <Lukeybooi@gmail.com>
2023-01-18 11:02:28 +0200

    fix: dateFormat camelCase issues resolved

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -87,12 +87,22 @@ export const getNumericValue = (localValue: any) => {
   }
 };
 export function updateQueryStringParameter(uri, key, value) {
-  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  var re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+  var separator = uri.indexOf('?') !== -1 ? '&' : '?';
   if (uri.match(re)) {
-    return uri.replace(re, '$1' + key + "=" + value + '$2');
+    return uri.replace(re, '$1' + key + '=' + value + '$2');
+  } else {
+    return uri + separator + key + '=' + value;
   }
-  else {
-    return uri + separator + key + "=" + value;
-  }
+}
+
+export function toCamelCase(str: string) {
+  return str
+    .replace(/\s(.)/g, function($1) {
+      return $1.toUpperCase();
+    })
+    .replace(/\s/g, '')
+    .replace(/^(.)/, function($1) {
+      return $1.toLowerCase();
+    });
 }
